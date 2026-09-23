aecd63134a6b940b0b4b01cbf235bef10ae38fa6
Said Querevalu <squerevalu@csi.edu>
2026-09-23 11:01:19 -0600

    Add HTML and JavaScript files for Week 6 Lab activities

---
+++ Week6/Lab6/scripts.js
@@ -0,0 +1,39 @@
+// Grate calculator
+
+let score = Number(prompt("Enter your score (0–100):"));
+if (score >= 90) {
+    console.log("The score you have is: A");
+}
+else if (score >= 80) {
+    console.log("The score you have is: B");
+}
+else if (score >= 70) {
+    console.log("The score you have is: C");
+}
+else {
+    console.log("The score you have is: F");
+}
+
+let age=Number(prompt("What is your age?"));
+if (age >= 18) {
+    console.log("Adult.");
+}
+else {
+    console.log("Minor.");
+}
+
+let discount=prompt("What membership do you have?").toUpperCase().toLowerCase();
+if (discount === "GOLD") {
+    console.log("You get a 45% discount.");
+}
+else if (discount === "silver") {
+    console.log("You get a 35% discount.");
+}
+else if (discount === "Bronze") {
+    console.log("You get a 20% discount.");
+}
+else {
+    console.log("Discount does not apply.");
+}
+
+
---
+++ Week6/Notes/scripts.js
@@ -0,0 +1,66 @@
+// comparision operators
+//=== equal to
+// !== not equal
+
+//console.log(1==1);
+//console.log(1!==1);
+
+//let score=85;
+//if (score >= 89) {
+//    console.log("You got an A");
+//}
+
+//else if (score >= 79) {
+//    console.log('You got a B');
+//}
+//else if (score >=69){
+//    console.log('You got a C or lower');
+//}
+//else {
+//    console.log('You have a mediocre grade');
+//}
+
+
+
+
+//let agre=25;
+//let isMember = true;
+
+//if (agre >= 18) {
+//    if(isMember) {
+//        console.log("adult member benefits applied");
+//    }
+//   else{
+//        console.log("Adult, but not a member");
+//    }
+    
+//} 
+//else {
+//        console.log("Minor");
+//    }
+
+
+
+//We use Prompt() to make our websites more interactive
+//let score=85;
+// //this variable is hardcoded. iser camt cjamge tjos
+//let score=Number(prompt("Enter your score (0-100)"));
+//if (score >= 90) {
+//    console.log("A");
+//}
+//else if (score >= 80) {
+//    console.log("B");
+//}
+//else if (score >= 70) {
+//    console.log("C");
+//}
+//else {
+//    console.log("F");
+//}
+
+//document.body.innerHTML += "<p>The score you have is a " + score + "</p>";
+
+
+//I want the website to ask for the user for their name, and then display "Hello [name]"
+let username=prompt("What is your name? ");
+document.body.innerHTML += "<p>Hello, " + username + "!</p>";
